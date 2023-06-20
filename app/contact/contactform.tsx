585e9ae349f8ac71abf2e58f0473aad93ea7eb88
Dani Laudza <danilaudza@gmail.com>
2023-06-20 21:47:56 +0700

    [ADD] contact form with react hook form

--- app/contact/contactform.tsx
+++ app/contact/contactform.tsx
@@ -1,76 +1,179 @@
 "use client"
 
 import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-import { cn } from "@/lib/utils"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { Icons } from "@/components/icons"
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .min(1, {
+      message: "Email has to be filled.",
+    })
+    .email({
+      message: "This is not a valid email.",
+    }),
+  message: z
+    .string()
+    .min(3, {
+      message: "Message should must be at least 3 characters.",
+    })
+    .max(255, {
+      message: "Message should must be max at 255 characters.",
+    }),
+})
 
-const ContactForm = ({ className, ...props }: UserAuthFormProps) => {
+const ContactForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
+  const [isSubmit, setisSubmit] = useState({
+    status: false,
+    msg: "",
+    head: "",
+  })
 
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      message: "",
+    },
+  })
+
+  const test = () => {
+    setisSubmit({
+      head: "",
+      msg: "",
+      status: !isSubmit.status,
+    })
   }
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true)
+    axios
+      .post("/api/contact", values)
+      .then((response) => {
+        setIsLoading(false)
+        setisSubmit({ head: "Done!", msg: "Message Submitted, Have a good day!", status: true })
+      })
+      .catch((err) => {
+        setIsLoading(false)
+        setisSubmit({ head: "Oops Sorry!", msg: err.message, status: true })
+      })
+  }
+
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-4">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                placeholder="Dani Laudza"
-                type="text"
-                autoCapitalize="none"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                placeholder="name@example.com"
-                type="email"
-                autoCapitalize="none"
-                autoComplete="email"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="message-2">Your Message</Label>
-              <Textarea
-                placeholder="Type your message here."
-                id="message-2"
-                className="max-h-64"
-                disabled={isLoading}
-              />
-              <p className="text-sm text-muted-foreground">
-                Your message will be copied to the support team.
-              </p>
-            </div>
-          </div>
-          <Button disabled={isLoading}>
+    <>
+      <AlertDialog open={isSubmit.status} onOpenChange={test}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{isSubmit.head}</AlertDialogTitle>
+            <AlertDialogDescription>{isSubmit.msg}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Ok</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Your name"
+                    type="text"
+                    autoCapitalize="none"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="name@example.com"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Type your message here."
+                    className="max-h-64"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isLoading}>
             {isLoading && (
               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             )}
             Submit
           </Button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </Form>
+    </>
   )
 }
 
