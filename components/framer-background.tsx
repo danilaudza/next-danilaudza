73a1b8701833d8d308eb1d674651478757306867
Dani Laudza <danilaudza@gmail.com>
2023-06-21 20:55:46 +0700

    [ADD] github workflow

--- components/framer-background.tsx
+++ components/framer-background.tsx
@@ -11,14 +11,14 @@ const FramerBackground = () => {
           rotate: [63, 63, 63],
         }}
         transition={{ repeat: Infinity, duration: 5, ease:"easeInOut"}}
-        className="absolute top-96 h-[20px] w-[800px] rotate-[63deg] transform rounded-full bg-red-600/50 blur-[70px] dark:bg-indigo-600/50 lg:left-96"
+        className="absolute top-96 h-[20px] w-[800px] rotate-[63deg] rounded-full bg-red-600/50 blur-[70px] dark:bg-indigo-600/50 lg:left-96"
       ></motion.div>
       <motion.div 
       animate={{
         rotate: [63, 80, 63],
       }}
       transition={{ repeat: Infinity, duration: 5, ease:"easeInOut"}}
-      className="absolute top-96 h-[20px] w-[600px] rotate-[63deg] transform rounded-full bg-green-600/50 blur-[70px] dark:bg-blue-600/50 lg:left-72"></motion.div>
+      className="absolute top-96 h-[20px] w-[600px] rotate-[63deg] rounded-full bg-green-600/50 blur-[70px] dark:bg-blue-600/50 lg:left-72"></motion.div>
     </div>
   )
 }
